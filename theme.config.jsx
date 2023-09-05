71a9f37430b987dbbebddbd131f17cb3eba41300
Zhengyu Qian <laerhsif@gmail.com>
2023-09-05 15:36:30 +0900

    feat(theme): config Open Graph meta tags

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,9 +1,36 @@
 import { useRouter } from 'next/router'
+import { useConfig, useTheme } from 'nextra-theme-docs'
 import { Logo } from '@components/logo'
 import { Footer } from '@components/footer'
 
 export default {
   head: () => {
+    const router = useRouter()
+    const { systemTheme = 'system' } = useTheme()
+    const { frontMatter } = useConfig()
+
+    const COPY_FALLBACK = {
+      title: 'The Digital Infrastructure of Global Hacker Movement',
+      desc: 'Dora Factory creates protocols, toolings, and public good infrastructures that help open source communities and frontier tech builders to thrive.'
+    }
+    // const SITE_ROOT = 'https://df-docs.vercel.app'
+    // const SITE_ROOT = 'http://localhost:3000'
+    const SITE_ROOT = process.env.SITE_ROOT
+    console.log('ENV')
+    console.log(process.env)
+    const asPath = router.asPath
+
+    let ogTitle = frontMatter.title || COPY_FALLBACK.title
+    let ogDesc = frontMatter.desc || COPY_FALLBACK.desc
+    let ogUrl = asPath === '/' ? SITE_ROOT : `${SITE_ROOT}${router.asPath}`
+    let ogImg = `${SITE_ROOT}/api/og`
+
+    if (asPath !== '/') {
+      const encTitle = encodeURIComponent(ogTitle)
+      const encDesc = encodeURIComponent(ogDesc)
+      ogImg = `${SITE_ROOT}/api/og?title=${encTitle}&desc=${encDesc}`
+    }
+
     return (
       <>
         <meta
@@ -30,6 +57,18 @@ export default {
           href="/assets/manifest.json"
           crossOrigin="use-credentials"
         />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en" />
+        <meta property="og:site_name" content="Dora Factory Documentation" />
+        <meta property="og:title" content={ogTitle} />
+        <meta property="og:description" content={ogDesc} />
+        <meta property="og:url" content={ogUrl} />
+        <meta property="og:image" content={ogImg} />
+        <meta property="twitter:image" content={ogImg} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@DoraFactory" />
+        <meta name="twitter:creator" content="@DoraFactory" />
       </>
     )
   },
