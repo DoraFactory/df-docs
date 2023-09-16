47e37d2f3b73561313110be5083d087040fbeb72
Zhengyu Qian <laerhsif@gmail.com>
2023-09-16 23:40:57 +0800

    feat(svg): add new icons
    + Checklist icon
    + CLI icon
    + GUI icon
    + Help icon

--- components/svgs/index.ts
+++ components/svgs/index.ts
@@ -1,1 +1,6 @@
 export { default as Lockup } from './lockup.svg'
+
+export { default as IconChecklist } from './icon-checklist.svg'
+export { default as IconCli } from './icon-cli.svg'
+export { default as IconGui } from './icon-gui.svg'
+export { default as IconHelp } from './icon-help.svg'
