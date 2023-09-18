a27934c5ca13474b5c35effe5ac87b1850129845
Zhengyu Qian <laerhsif@gmail.com>
2023-09-18 20:33:31 +0800

    feat(svg): add icon of `IconCode`

--- components/svgs/index.ts
+++ components/svgs/index.ts
@@ -2,6 +2,7 @@ export { default as Lockup } from './lockup.svg'
 
 export { default as IconChecklist } from './icon-checklist.svg'
 export { default as IconCli } from './icon-cli.svg'
+export { default as IconCode } from './icon-code.svg'
 export { default as IconGui } from './icon-gui.svg'
 export { default as IconHelp } from './icon-help.svg'
 export { default as IconReference } from './icon-reference.svg'
